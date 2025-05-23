9bed3b280dfc598ff4d41364f4b0806001ba19b7
Jake Alexander Murray <j.murray@premierse.com>
2025-05-23 18:32:23 +0800

    Add VisuallyHidden component

---
+++ Blazix/VisuallyHidden/VisuallyHiddenRoot.razor.cs
@@ -0,0 +1,9 @@
+namespace Blazix.VisuallyHidden;
+
+/// <summary>
+/// Anything you put inside this component will be hidden from the screen but will be announced by screen readers.
+/// </summary>
+public partial class VisuallyHiddenRoot
+{
+
+}
