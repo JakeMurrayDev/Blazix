b0e44c8631162fbb9e4e3afe148011fee373eaa2
Jake Alexander Murray <j.murray@premierse.com>
2025-05-23 23:39:30 +0800

    Add summaries to progress component

---
+++ Blazix/Progress/ProgressIndicator.razor.cs
@@ -0,0 +1,9 @@
+namespace Blazix.Progress;
+
+/// <summary>
+/// Used to show the progress visually. It also makes progress accessible to assistive technologies.
+/// </summary>
+public partial class ProgressIndicator
+{
+
+}
---
+++ Blazix/Progress/ProgressRoot.razor.cs
@@ -0,0 +1,14 @@
+namespace Blazix.Progress;
+
+/// <summary>
+/// <para>
+/// Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.
+/// </para>
+/// <para>
+/// Contains all of the progress parts.
+/// </para>
+/// </summary>
+public partial class ProgressRoot
+{
+
+}
