7d9c08a8901d822dde3e9f2f15ff15ab362f4f97
Jake Alexander Murray <j.murray@premierse.com>
2025-05-23 18:43:39 +0800

    Refactor checkbox component and add summaries

---
+++ Blazix/Checkbox/CheckboxIndicator.razor.cs
@@ -0,0 +1,9 @@
+namespace Blazix.Checkbox;
+
+/// <summary>
+/// Renders when the checkbox is in a checked or indeterminate state. You can style this element directly, or you can use it as a wrapper to put an icon into, or both.
+/// </summary>
+public partial class CheckboxIndicator
+{
+
+}
---
+++ Blazix/Checkbox/CheckboxRoot.razor.cs
@@ -0,0 +1,9 @@
+namespace Blazix.Checkbox;
+
+/// <summary>
+/// Contains all the parts of a checkbox. A <see cref="NullableInputCheckbox"/> will also render when used within an <see cref="EditForm"/> to ensure events propagate correctly.
+/// </summary>
+public partial class CheckboxRoot
+{
+
+}
