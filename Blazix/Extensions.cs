ccaff9df582100a0793812ea3aca5a15048738a4
Jake Alexander Murray <j.murray@premierse.com>
2025-05-29 13:29:57 +0800

    Add reading direction service

---
+++ Blazix/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Blazix.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazix;
+
+/// <summary>
+/// Extension methods for registering Blazix components and services.
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Registers all Blazix components and services.
+    /// </summary>
+    public static void AddBlazix(this IServiceCollection services)
+    {
+        services.AddSingleton<IReadingDirectionService, ReadingDirectionService>();
+    }
+}
---
+++ Blazix/Services/ReadingDirectionService.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Blazix.Services;
+
+internal sealed class ReadingDirectionService(ReadingDirection? readingDirection = null) : IReadingDirectionService
+{
+    private static ReadingDirection GetReadingDirectionFromCulture() => CultureInfo.CurrentCulture.TextInfo.IsRightToLeft ? ReadingDirection.RightToLeft : ReadingDirection.LeftToRight;
+    public ReadingDirection GetReadingDirection() => readingDirection ?? GetReadingDirectionFromCulture();
+}
+
+public interface IReadingDirectionService
+{
+    ReadingDirection GetReadingDirection();
+}
