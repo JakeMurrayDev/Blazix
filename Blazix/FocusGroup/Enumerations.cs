ca6b5f8a39e7d2e79df8ae4e606140ffbf3de7fe
Jake Alexander Murray <j.murray@premierse.com>
2025-05-29 13:30:18 +0800

    Add focus group utility component

---
+++ Blazix/FocusGroup/Enumerations.cs
@@ -0,0 +1,24 @@
+﻿namespace Blazix.FocusGroup;
+
+/// <summary>
+/// The navigation intent of the focused element.
+/// </summary>
+public enum FocusIntent
+{
+    /// <summary>
+    /// Focus the first element.
+    /// </summary>
+    First,
+    /// <summary>
+    /// Focus the last element.
+    /// </summary>
+    Last,
+    /// <summary>
+    /// Focus the previous element.
+    /// </summary>
+    Prev,
+    /// <summary>
+    /// Focus the next element.
+    /// </summary>
+    Next
+}
