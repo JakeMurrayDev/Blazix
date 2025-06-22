c712ba13e66c8442f3dbc9ab1505cb371ea70454
Jake Alexander Murray <j.murray@premierse.com>
2025-06-22 15:22:35 +0800

    Refactor blazix-checkbox to use blazix-utilities

--- Blazix/wwwroot/blazix-checkbox.js
+++ Blazix/wwwroot/blazix-checkbox.js
@@ -1,23 +1,6 @@
-/**
- * Sets the size of the target input element to match the source button element.
- * @param {HTMLElement} buttonElement - The source button element.
- * @param {HTMLElement} inputElement - The target input element to resize.
- */
-export function syncInputSize(buttonElement, inputElement) {
-    if (buttonElement && inputElement) {
-        try {
-            const width = buttonElement.offsetWidth;
-            const height = buttonElement.offsetHeight;
-            inputElement.style.width = `${width}px`;
-            inputElement.style.height = `${height}px`;
-        } catch (e) {
-            console.error("Blazix Checkbox: Error getting button size:", e);
-        }
-    } else {
-        if (!buttonElement) console.warn("Blazix Checkbox: syncInputSize called without buttonElement.");
-        if (!inputElement) console.warn("Blazix Checkbox: syncInputSize called without inputElement.");
-    }
-}
+import { syncInputSize } from './blazix-utilities.js';
+
+export { syncInputSize };
 
 /**
  * Programmatically updates the state of a checkbox input and dispatches
