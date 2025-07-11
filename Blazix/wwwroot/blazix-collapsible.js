a9c7f675d1ac754de0dfd7ca354c245dacc4c503
Jake Alexander Murray <j.murray@premierse.com>
2025-07-11 17:53:56 +0800

    Fix collapsible animating on first render when it should not

--- Blazix/wwwroot/blazix-collapsible.js
+++ Blazix/wwwroot/blazix-collapsible.js
@@ -1,21 +1,30 @@
-export function scheduleAnimationFrame(dotnetHelper) {
-    const rAF = requestAnimationFrame(() => {
-        dotnetHelper.invokeMethodAsync('SetMountAnimationPrevented');
-    });
-    return rAF;
-}
+const BLAZIX_STATE_KEY = Symbol.for("Blazix.Collapsible.State");
 
-export function cancelScheduledAnimationFrame(rAF) {
-    cancelAnimationFrame(rAF);
-}
-
-export function updateCollapsibleContentSize(element, isMountAnimationPrevented) {
-    if (!element) return { Height: 0, Width: 0 };
-
-    const originalStyles = {
-        transitionDuration: element.style.transitionDuration,
-        animationName: element.style.animationName,
+if (!window[BLAZIX_STATE_KEY]) {
+    window[BLAZIX_STATE_KEY] = {
+        originalStyles: new Map(),
     };
+}
+
+const state = window[BLAZIX_STATE_KEY];
+
+/**
+ * Processes the animation rendering and size of an element
+ * @param {string} id - The id of the element
+ * @param {HTMLElement} element - The element to process
+ * @param {Boolean} firstRender - Whether this is the first render of the element
+ * @returns
+ */
+export function processAnimationAndSize(id, element, firstRender) {
+
+    if (!element) return { width: 0, height: 0 };
+
+    if (!state.originalStyles.has(id)) {
+        state.originalStyles.set(id, {
+            transitionDuration: element.style.transitionDuration,
+            animationName: element.style.animationName,
+        });
+    }
 
     element.style.transitionDuration = '0s';
     element.style.animationName = 'none';
@@ -24,10 +33,20 @@ export function updateCollapsibleContentSize(element, isMountAnimationPrevented)
     const height = rect.height;
     const width = rect.width;
 
-    if (!isMountAnimationPrevented) {
-        element.style.transitionDuration = originalStyles.transitionDuration;
-        element.style.animationName = originalStyles.animationName;
-    }
+    element.style.setProperty('--blazix-collapsible-content-height', `${height}px`);
+    element.style.setProperty('--blazix-collapsible-content-width', `${width}px`);
 
-    return { Width: width, Height: height };
+    void element.offsetWidth;
+
+    if (state.originalStyles.has(id) && !firstRender) {
+        var { transitionDuration, animationName } = state.originalStyles.get(id);
+        element.style.transitionDuration = transitionDuration;
+        element.style.animationName = animationName;
+    }
+}
+
+export function dispose(id) {
+    if (state.originalStyles.has(id)) {
+        state.originalStyles.delete(id);
+    }
 }
