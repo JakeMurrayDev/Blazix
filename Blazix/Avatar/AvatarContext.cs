73df412f480d1f9503a28440af58fe51eed6ad4d
jamurraydeveloper <jamurraydeveloper@gmail.com>
2025-11-29 00:43:55 +0800

    Code cleanup in avatar component

--- Blazix/Avatar/AvatarContext.cs
+++ Blazix/Avatar/AvatarContext.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿namespace Blazix.Avatar;
 
-namespace Blazix.Avatar;
-
-internal class AvatarContext
+/// <summary>
+/// The context for the <see cref="AvatarRoot"/> component.
+/// </summary>
+internal sealed class AvatarContext
 {
     /// <summary>
     /// The status of the <see cref="AvatarImage"/> being loaded.
     /// </summary>
-    internal ImageLoadingStatus Status { get; private set; } = ImageLoadingStatus.Idle;
+    public ImageLoadingStatus Status { get; private set; } = ImageLoadingStatus.Idle;
 
     /// <summary>
     /// Action to notify subscribers (<see cref="AvatarImage"/> and <see cref="AvatarFallback"/>) about status changes.
     /// </summary>
-    internal Func<ImageLoadingStatus, Task>? OnStatusChange { get; set; }
+    public Func<ImageLoadingStatus, Task>? OnStatusChange { get; set; }
 
     /// <summary>
     /// Sets the status of the <see cref="AvatarImage"/> being loaded
     /// </summary>
     /// <param name="newStatus">The new status.</param>
-    internal async Task SetStatusAsync(ImageLoadingStatus newStatus)
+    public async Task SetStatusAsync(ImageLoadingStatus newStatus)
     {
         if (Status != newStatus)
         {
