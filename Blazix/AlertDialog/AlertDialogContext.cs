bec6f86216c8044c06934156443172242d75158a
jamurraydeveloper <jamurraydeveloper@gmail.com>
2025-11-30 19:48:31 +0800

    Add alert dialog component

---
+++ Blazix/AlertDialog/AlertDialogContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazix.AlertDialog;
+
+internal sealed class AlertDialogContentContext
+{
+    public ElementReference? CancelElement { get; private set; }
+
+    public void SetCancelElement(ElementReference? element) => CancelElement = element;
+}
