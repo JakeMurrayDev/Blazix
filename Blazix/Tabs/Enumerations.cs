aa2e43d258d13a9dd5a66729bf030f4c63893cfa
jamurraydeveloper <jamurraydeveloper@gmail.com>
2025-12-03 22:37:03 +0800

    Update tabs documentation

--- Blazix/Tabs/Enumerations.cs
+++ Blazix/Tabs/Enumerations.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Blazix.Tabs;
 
-namespace Blazix.Tabs;
-
+/// <summary>
+/// Defines the activation modes for tab components.
+/// </summary>
 public enum ActivationMode
 {
+    /// <summary>
+    /// Tabs are activated automatically when receiving focus.
+    /// </summary>
     Automatic,
+    /// <summary>
+    /// Tabs are activated when clicked.
+    /// </summary>
     Manual
 }
